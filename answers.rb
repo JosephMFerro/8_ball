1ae67dc66c9121083cb6c1269a40a4ae0747512f
Colton Davis <colt822lavay@outlook.com>
2018-11-08 14:43:09 -0700

    Add class EightBall and method ask

--- answers.rb
+++ answers.rb
@@ -1,14 +1,4 @@
 
-
-# def random_answer
-#   @hash = {1 => 'Yes', 2 =>'Not Likely', 3 => 'I Cannot Say'}
-#   n = 0
-#   @ans = @hash.to_a.sample(1).to_h
-#   puts '#{@ans}'
-# end
-
-# random_answer
-
 @answers = [
   "Certainly",
   "Unlikely",
